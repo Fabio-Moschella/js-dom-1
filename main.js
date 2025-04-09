c2d551a69a65f7ed2b63f845bec6d1fe09bc777f
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-04-09 17:29:58 +0200

    add-bonus-track

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 const powerButton = document.getElementById("buttonOnAndOff");
 const lightOn = document.getElementById("lightOn");
+const lightOff = document.getElementById("lightOff");
+
+console.log(powerButton);
+console.log(lightOn);
+console.log(lightOff);
+
+// powerButton.addEventListener("click", function () {
+//   lightOn.classList.remove("d-none");
+//   lightOn.classList.add("d-block");
+// });
 
 powerButton.addEventListener("click", function () {
-  lightOn.classList.remove("d-none");
-  lightOn.classList.add("d-block");
+  if (lightOff.classList.contains("d-inline-block")) {
+    lightOff.classList.replace("d-inline-block", "d-none");
+    lightOn.classList.replace("d-none", "d-inline-block");
+    powerButton.classList.replace("btn-primary", "btn-danger");
+  } else {
+    lightOff.classList.replace("d-none", "d-inline-block");
+    lightOn.classList.replace("d-inline-block", "d-none");
+    powerButton.classList.replace("btn-danger", "btn-primary");
+  }
 });
